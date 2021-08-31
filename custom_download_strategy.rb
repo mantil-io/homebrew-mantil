bbf504746f7f228449a29aa3f5082ef2daef6cd0
Ivan Vlasic <ivan@atoz.technology>
2021-08-31 11:25:50 +0200

    fix download strategy

--- custom_download_strategy.rb
+++ custom_download_strategy.rb
@@ -1,16 +1,8 @@
 require "download_strategy"
 
 # S3DownloadStrategy downloads tarballs from AWS S3.
-# To use it, add `:using => :s3` to the URL section of your
-# formula.  This download strategy uses AWS access tokens (in the
-# environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`)
-# to sign the request.  This strategy is good in a corporate setting,
-# because it lets you use a private S3 bucket as a repo for internal
-# distribution.  (It will work for public buckets as well.)
 class S3DownloadStrategy < CurlDownloadStrategy
   def initialize(url, name, version, **meta)
-    odeprecated("S3DownloadStrategy",
-      "maintaining S3DownloadStrategy in your own formula or tap")
     super
   end
 
@@ -23,17 +15,6 @@ class S3DownloadStrategy < CurlDownloadStrategy
     bucket = Regexp.last_match(1)
     key = Regexp.last_match(2)
 
-    ENV["AWS_ACCESS_KEY_ID"] = ENV["HOMEBREW_AWS_ACCESS_KEY_ID"]
-    ENV["AWS_SECRET_ACCESS_KEY"] = ENV["HOMEBREW_AWS_SECRET_ACCESS_KEY"]
-
-    begin
-      signer = Aws::S3::Presigner.new
-      s3url = signer.presigned_url :get_object, bucket: bucket, key: key
-    rescue Aws::Sigv4::Errors::MissingCredentialsError
-      ohai "AWS credentials missing, trying public URL instead."
-      s3url = url
-    end
-
-    curl_download s3url, to: temporary_path
+    curl_download url, to: temporary_path
   end
 end
