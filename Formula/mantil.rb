af17b43c3ea9a5b7f1316bdf668cd349febf4798
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-20 21:41:51 +0200

    Brew formula update for mantil version v0.1.5

--- Formula/mantil.rb
+++ Formula/mantil.rb
@@ -5,18 +5,18 @@
 class Mantil < Formula
   desc "Mantil CLI"
   homepage ""
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://mantil-downloads.s3.eu-central-1.amazonaws.com/releases/cli/0.1.4/mantil_0.1.4_Darwin_x86_64.tar.gz"
-      sha256 "c8b03cc930a40a0246cf20ef91ebb520dba97edd9de2350da90535e3ba9cc0f0"
+      url "https://mantil-downloads.s3.eu-central-1.amazonaws.com/releases/cli/0.1.5/mantil_0.1.5_Darwin_x86_64.tar.gz"
+      sha256 "4fd59390b2ed68f18bf195e8547d4aae093499cf4c56953c8d38f46c96500de1"
     end
     if Hardware::CPU.arm?
-      url "https://mantil-downloads.s3.eu-central-1.amazonaws.com/releases/cli/0.1.4/mantil_0.1.4_Darwin_arm64.tar.gz"
-      sha256 "ff052b7c76233266caa74a0489223bcc9a4626ae6e044c8ff89e8db63b101063"
+      url "https://mantil-downloads.s3.eu-central-1.amazonaws.com/releases/cli/0.1.5/mantil_0.1.5_Darwin_arm64.tar.gz"
+      sha256 "93f5ba03aa866add57c5653b9a61b0151100e5e9694a62b45bd756dd369a5e38"
     end
   end
 
